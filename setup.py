40520397cebdedca1b87bcf845c877fb9dd07e67
adnananouzla <adnan.anouzla@enim.ac.ma>
2025-01-01 21:56:29 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Adnan ANOUZLA',
+    author_email= 'adnananouzla1@gmail.com',
+    packages= find_packages(), # Help to install src packages
+    install_requires=[]
+
+)
